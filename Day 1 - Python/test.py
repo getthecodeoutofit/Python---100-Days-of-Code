607a2a9a667281fa7bc673ee5a8362d623b9ee9f
Ravi Mahar <ravimahar12321@gmail.com>
2023-09-02 22:05:46 +0530

    "added day2"

---
+++ Day 1 - Python/test.py
@@ -0,0 +1,5 @@
+print("hello"+" world!")
+
+#input function taking
+
+
---
+++ Day-2 Python/test.py
@@ -0,0 +1,21 @@
+#primitive data types
+#string,int = 123_324 here the interpreter automatically remove the under score,
+#float
+#boolean True/False
+'''
+ len function
+type checking using type() function
+type casting - new_variable =  data_type(old variable to convert)
+
+round()  function
+we can add the precision according to descimal point
+just like
+round(2.333,2)
+here we are rounding off the 2.333 with 2 decimal point and the anser will be 2.32
+
+f-string
+f"hello your score us {variable_name}"
+
+
+
+'''
