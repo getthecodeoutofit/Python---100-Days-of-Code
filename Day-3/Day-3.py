6e5b0748d3fb4fbf287711ef5806f29521e0e5f9
Ravi Mahar <ravimahar12321@gmail.com>
2023-09-03 23:13:20 +0530

    "Day-4"

---
+++ Day-3/Day-3.py
@@ -0,0 +1,31 @@
+'''
+int the lecture we had studied about the if else conditional statements 
+using of nested if/ else 
+
+using elif:
+etc
+
+Logical operator: 
+and
+or
+not (reverses the condition)
+
+
+changing cases :
+variablestring.upper()
+variablestring.lower()
+
+count fn()
+
+variablestring.count('char')
+it count the accourance of the character in the variable string
+
+how to use double quotes inside double quotes
+it will se it as code "any word" inside the message
+the "\" sysmbol used to escape the problem
+
+
+
+
+'''
+input("hello\"typeit\"")#\ is use to remove that problem
---
+++ Day-4/Day-4.py
@@ -0,0 +1,40 @@
+'''
+RANDOMIZATION IN PYTHON;:::::
+
+(the mersenne twister)
+
+first import the random module
+
+random.rindint(from,to)
+
+
+.......................................................................
+Python list (data structure)
+
+a = []
+mutable 
+indexing allowed ,negative index
+
+
+
+'''
+import random
+print(random.randint(1,2))
+print(random.random())# randon fx is use to print the random floating number bw 0 and 1 only
+
+a = []
+a.append('hello')
+
+print(a)
+
+'''
+spit( ) is used to split the string
+
+
+len(list) this function is used to get the length of string or any list touple dict etc
+
+nested list possible
+list[list1,list2]
+
+
+'''
