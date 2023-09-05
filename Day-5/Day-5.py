d7997398fd7c36ae6b9364ea0e68b33b699b19aa
Ravi Mahar <ravimahar12321@gmail.com>
2023-09-05 07:40:04 +0530

    "Day-5"

---
+++ Day-5/Day-5.py
@@ -0,0 +1,22 @@
+'''
+concept of loop
+
+loop in a list using the name and for loop
+
+split()
+
+
+random.choice() this choose random fn
+'''
+
+a = ['hello','hi','how','when']
+for i in a:
+    print(i,'\n')
+s = 'hello'
+b = s.split()#used to change string into function
+print(b)
+
+n = int(input("enter the number you want to enter"))
+for i in range(0,n): 
+    print(i,end=" ")
+#if we add another , value in range it will means the step /skip
