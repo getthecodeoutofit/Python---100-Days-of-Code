743849686c88539aafd5bf3c60d247b9266f09af
Ravi Mahar <ravimahar12321@gmail.com>
2023-09-24 17:29:54 +0530

    "updated"

---
+++ Day-6/Day-6.py
@@ -0,0 +1,11 @@
+'''
+python functions
+
+user defined
+
+def Function_name():
+    writter here in check with the indent
+    always use space over tabs
+    
+while loop
+'''
---
+++ Day-8/Day-8.py
@@ -0,0 +1,7 @@
+'''
+functions with arguments......
+ 
+ 
+
+'''
+
